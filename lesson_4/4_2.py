fb4fd6b5a96cc26f0200f922056bb794ff84036a
levkovets <vlada.levkovets@yandex.ru>
2022-06-24 10:40:05 +0300

    task 2

---
+++ lesson_4/4_2.py
@@ -0,0 +1,19 @@
+def my_fun():
+    a = input("Введите строку: ").lower()
+    a_set = set(a)
+    max_len = 0
+
+    for c in a_set:
+        if a.count(c) > max_len:
+            max_len = a.count(c)
+
+    a_len = len(str(max_len))
+
+    for c in a_set:
+        if not c.isalpha():
+            continue
+
+        print("| {} | {:<{}} |".format(c, a.count(c), a_len))
+
+
+my_fun()
