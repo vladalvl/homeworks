f7677ce683b5bb8d90f03943a8cca27604bc2c98
levkovets <vlada.levkovets@yandex.ru>
2022-07-04 15:35:26 +0300

    задание 1

--- lesson_3/3_1.py
+++ lesson_3/3_1.py
@@ -1,12 +1,16 @@
 a = input("Введите число: ").replace(',', '.')
 
-if a.isdigit() or a.count("-") <= 1 and a.count(".") <= 1 or a[0] == '-' and a[1:].isdigit():
-    a = float(a)
-    if a > 0:
-        print("Положительное")
-    elif a == 0:
-        print("Ноль")
-    elif a < 0:
-        print("Отрицательное")
+
+if a.count('-') <= 1 and a[0] == '-' and a.count('.') <= 1:
+    if a.replace('.', '').replace('-', '').isdigit():
+        a = float(a)
+        if a > 0:
+            print("Положительное")
+        elif a == 0:
+            print("Ноль")
+        elif a < 0:
+            print("Отрицательное")
+    else:
+        print("Введите число!")
 else:
-    print("Введите число!")
+    print("Попробуй еще раз!")
