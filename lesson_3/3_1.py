948273fa6c653faef7fbf2b7c57ecd80a8a621b4
vl.levkovets <vlada.levkovets@yandex.ru>
2022-08-09 17:42:12 +0300

    задание 1

--- lesson_3/3_1.py
+++ lesson_3/3_1.py
@@ -1,16 +1,39 @@
 a = input("Введите число: ").replace(',', '.')
 
 
-if a.count('-') <= 1 and a[0] == '-' and a.count('.') <= 1:
-    if a.replace('.', '').replace('-', '').isdigit():
-        a = float(a)
-        if a > 0:
-            print("Положительное")
-        elif a == 0:
-            print("Ноль")
-        elif a < 0:
-            print("Отрицательное")
-    else:
-        print("Введите число!")
-else:
+if a.count("." and "-") > 1:
     print("Попробуй еще раз!")
+else:
+    if '-' or '' in a[0]:
+        if a.replace('.', '').replace('-', '').isdigit():
+            a = float(a)
+            if a > 0:
+                print("Положительное")
+            elif a == 0:
+                print("Ноль")
+            elif a < 0:
+                print("Отрицательное")
+            else:
+                print("Введите число!")
+    else:
+        print("Необходимо ввести число!")
+
+# Через try/except
+
+# b = input("Введите число: ").replace(',', '.')
+#
+#
+# try:
+#
+#     b = float(b)
+#     if b > 0:
+#         print("Положительное")
+#     elif b == 0:
+#         print("Ноль")
+#     elif b < 0:
+#         print("Отрицательное")
+#     else:
+#         print("Введите число!")
+#
+# except ValueError:
+#     print("Попробуйте еще раз!")
